00f5dc27d9dd8ff62180099d31596c881508065e
majaliju <majalijunyc@gmail.com>
2022-12-21 18:40:35 -0500

    at least it works; need to fix serailizers tho

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -2,6 +2,6 @@ class ArtistSerializer < ActiveModel::Serializer
   attributes :id, :name, :image, :genre
 
 
-  has_many :concerts, serializer: ConcertSerializer
-  has_many :posts, through: :concerts, serializer: PostSerializer
+  # has_many :concerts, serializer: ConcertSerializer
+  # has_many :posts, through: :concerts, serializer: PostSerializer
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email
 
 
-  has_many :posts, serializer: PostSerializer
+  # has_many :posts, serializer: PostSerializer
 end
